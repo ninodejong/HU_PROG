ae0425c5256c087c9088d9056d6560107fd3cd1c
ninodejong <ninodejong@users.noreply.github.com>
2018-09-20 15:22:31 +0200

    init

---
+++ pe2_1.py
@@ -0,0 +1,7 @@
+# from collections import Counter
+# letters = ('A', 'C', 'B', 'B', 'C', 'A', 'C', 'C', 'B')
+# print(Counter(letters))
+letters = ('A', 'C', 'B', 'B', 'C', 'A', 'C', 'C', 'B')
+list1 = [letters.count("A"),letters.count("B"),letters.count("C")]
+
+print(list1)
